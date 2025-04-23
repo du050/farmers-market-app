a126a660d13a6ca2c970c277c05446bcb1e2e00e
du050 <Dudacintraxwork@gmail.com>
2025-04-22 20:56:01 -0600

    Add MapClient component and integrate Leaflet for map functionality

---
+++ src/app/search/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import MapClient from '@/components/layout/MapClient';
+export default function SearchPage() {
+  return (
+    <div>
+      <h1>Search Page</h1>
+      <MapClient />
+    </div>
+  );
+}
---
+++ src/components/layout/MapClient.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+delete (L.Icon.Default.prototype as unknown as { _getIconUrl?: () => void })._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.3/dist/images/marker-icon-2x.png',
+  iconUrl: 'https://unpkg.com/leaflet@1.9.3/dist/images/marker-icon.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.3/dist/images/marker-shadow.png',
+});
+
+export default function MapClient() {
+  return (
+    <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: '400px', width: '100%' }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>Hello from Leaflet!</Popup>
+      </Marker>
+    </MapContainer>
+  );
+}
